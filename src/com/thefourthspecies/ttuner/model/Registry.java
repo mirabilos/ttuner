bbabb3a495cc8166af49bdc20968aa29e67175e8
mirabilos <t.glaser@tarent.de>
2018-04-03 07:38:01 +0200

    normalise Bach/Lehman 1722 and add Bach/Lehman Chorale

--- src/com/thefourthspecies/ttuner/model/Registry.java
+++ src/com/thefourthspecies/ttuner/model/Registry.java
@@ -49,9 +49,15 @@ public class Registry {
 				"C=E, C=G-1/4S, G=D-1/4S, D=A-1/4S, A=E-1/4S, " +
 				"E=B, B=F#, Db=Ab, Ab=Eb, Eb=Bb, Bb=F, F=C");
 		
-		addDefaultTemperament("Lehman-Bach",
-				"A = E-1/6P, E = B, D = A-1/6P, G = D-1/6P, C = G-1/6P, F = C-1/6P, " +
-				"Bb = F+1/12P, Eb = Ab-1/12P, Ab = C#-1/12P, F# = B, C# = F#");
+		addDefaultTemperament("Bach/Lehman 1722",
+				"C = F-1/6P, G = C-1/6P, D = G-1/6P, A = D-1/6P, " +
+				"E = A-1/6P, B = E, F# = B, C# = F#, G# = C#-1/12P, " +
+				"D# = G#-1/12P, A# = D#-1/12P, F = Bb+1/12P");
+
+		addDefaultTemperament("Bach/Lehman Chorale",
+				"D = G-1/6P, A = D-1/6P, E = A-1/6P, B = E-1/6P, " +
+				"F# = B-1/6P, C# = F#, G# = C#, D# = G#, Bb = Eb-1/12P, " +
+				"F = Bb-1/12P, C = F-1/12P, G = C+1/12P");
 		
 		addDefaultTemperament("Meantone, 1/4 Comma",
 				"A = F, Bb = D, B = G, B= E-1/4S, C = E, C# = A, C#= F#-1/4S, D = F#, " +
